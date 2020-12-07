8bc3c3e26e5d0044397df6fcdb94718669d4d612
michael.zikes <michael.zikes@intauria.com>
2020-12-07 18:40:44 +0100

    Init

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/flight-app',
+    '<rootDir>/apps/dashboard',
+    '<rootDir>/apps/flight-admin',
+    '<rootDir>/libs/logger-lib',
+    '<rootDir>/libs/flight-lib',
+    '<rootDir>/libs/dashboard-lib',
+  ],
+};
